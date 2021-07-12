b8a95d08274aa9c7701c4ec1fa620a54569c1063
lona9 <pilar.vasquez.h@gmail.com>
2021-07-12 11:46:03 -0400

    fix cross server interaction for logs

--- lib/cogs/log.py
+++ lib/cogs/log.py
@@ -20,72 +20,74 @@ class Log(Cog):
   @Cog.listener()
   async def on_member_update(self, before, after):
 
-      tz_CL = pendulum.timezone('America/Santiago')
-      time_now = datetime.now(tz_CL)
+      if after.guild.id == 855476704978010132:
+          tz_CL = pendulum.timezone('America/Santiago')
+          time_now = datetime.now(tz_CL)
 
-      if before.display_name != after.display_name:
-        embed = Embed(title="Member update", description="Nickname change", timestamp=time_now)
+          if before.display_name != after.display_name:
+            embed = Embed(title="Member update", description="Nickname change", timestamp=time_now)
 
-        fields = [("Before", before.display_name, False),
-                  ("After", after.display_name, False),
-                  ("Member ID", after.id, False)]
+            fields = [("Before", before.display_name, False),
+                      ("After", after.display_name, False),
+                      ("Member ID", after.id, False)]
 
-        for name, value, inline in fields:
-          embed.add_field(name=name, value=value, inline=inline)
+            for name, value, inline in fields:
+              embed.add_field(name=name, value=value, inline=inline)
 
-        embed.set_author(name='pececito', icon_url=after.guild.icon_url)
+            embed.set_author(name='pececito', icon_url=after.guild.icon_url)
 
-        await self.log_channel.send(embed=embed)
+            await self.log_channel.send(embed=embed)
 
-      if before.roles != after.roles:
-        embed = Embed(title="Member update", description= f"Role change: user **{after.display_name}#{after.discriminator}**", timestamp=time_now)
+          if before.roles != after.roles:
+            embed = Embed(title="Member update", description= f"Role change: user **{after.display_name}#{after.discriminator}**", timestamp=time_now)
 
-        fields = [("Before", ", ".join([r.mention for r in before.roles]), False),
-                ("After", ", ".join([r.mention for r in after.roles]), False),
-                ("Member ID", after.id, False)]
+            fields = [("Before", ", ".join([r.mention for r in before.roles]), False),
+                    ("After", ", ".join([r.mention for r in after.roles]), False),
+                    ("Member ID", after.id, False)]
 
-        for name, value, inline in fields:
-          embed.add_field(name=name, value=value, inline=inline)
+            for name, value, inline in fields:
+              embed.add_field(name=name, value=value, inline=inline)
 
-        embed.set_author(name='pececito', icon_url=after.guild.icon_url)
+            embed.set_author(name='pececito', icon_url=after.guild.icon_url)
 
-        await self.log_channel.send(embed=embed)
+            await self.log_channel.send(embed=embed)
 
   @Cog.listener()
   async def on_member_remove(self, member):
 
-      tz_CL = pendulum.timezone('America/Santiago')
-      time_now = datetime.now(tz_CL)
+      if member.guild.id == 855476704978010132:
+          tz_CL = pendulum.timezone('America/Santiago')
+          time_now = datetime.now(tz_CL)
 
-      embed = Embed(title="Member remove", timestamp=time_now)
+          embed = Embed(title="Member remove", timestamp=time_now)
 
-      fields = [("This member left the server", f"{member}", False),
-                ("Member ID", member.id, False)]
+          fields = [("This member left the server", f"{member}", False),
+                    ("Member ID", member.id, False)]
 
-      for name, value, inline in fields:
-        embed.add_field(name=name, value=value, inline=inline)
+          for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
 
-      embed.set_author(name='pececito', icon_url=self.guild.icon_url)
+          embed.set_author(name='pececito', icon_url=self.guild.icon_url)
 
-      await self.log_channel.send(embed=embed)
+          await self.log_channel.send(embed=embed)
 
   @Cog.listener()
   async def on_member_join(self, member):
+      if member.guild.id == 855476704978010132:
+          tz_CL = pendulum.timezone('America/Santiago')
+          time_now = datetime.now(tz_CL)
 
-      tz_CL = pendulum.timezone('America/Santiago')
-      time_now = datetime.now(tz_CL)
+          embed = Embed(title="Member join", timestamp=time_now)
 
-      embed = Embed(title="Member join", timestamp=time_now)
+          fields = [("This member joined the server", f"{member}", False),
+                    ("Member ID", member.id, False)]
 
-      fields = [("This member joined the server", f"{member}", False),
-                ("Member ID", member.id, False)]
+          for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
 
-      for name, value, inline in fields:
-        embed.add_field(name=name, value=value, inline=inline)
+          embed.set_author(name='pececito', icon_url=self.guild.icon_url)
 
-      embed.set_author(name='pececito', icon_url=self.guild.icon_url)
-
-      await self.log_channel.send(embed=embed)
+          await self.log_channel.send(embed=embed)
 
 def setup(bot):
   bot.add_cog(Log(bot))
